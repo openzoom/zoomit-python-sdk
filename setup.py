010b3df78d60cc59f3c3944e44b4de9e8cecb64b
Daniel Gasienica <daniel@gasienica.ch>
2011-06-12 21:18:57 -0700

    Add `setup.py`.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name='zoomit-python-sdk',
+      version='0.1',
+      description='This client library is designed to support the Zoom.it API.',
+      author='Daniel Gasienica',
+      author_email="daniel@gasienica.ch",
+      url='http://github.com/openzoom/zoomit-python-sdk',
+      package_dir={'': 'src'},
+      py_modules=['zoomit'])
